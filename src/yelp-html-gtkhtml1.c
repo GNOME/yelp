1ec6126a958bcaf91b54f22eeac16d136139b2fb
Mikael Hallendal <micke@codefactory.se>
2002-05-22 22:40:11 +0000

    - don't shortcut info
    2002-05-23  Mikael Hallendal  <micke@codefactory.se>
    
    	* src/yelp-html-gtkhtml1.c (html_link_clicked_cb):
    	- don't shortcut info

--- src/yelp-html-gtkhtml1.c
+++ src/yelp-html-gtkhtml1.c
@@ -201,8 +201,7 @@ html_link_clicked_cb (GtkHTML *html_view, const gchar *url, YelpHtml *html)
 	if (yelp_uri_equal_path (uri, priv->base_uri) &&
 	    yelp_uri_get_section (uri)) {
 		if (yelp_uri_get_type (uri) == YELP_URI_TYPE_HTML ||
-		    yelp_uri_get_type (uri) == YELP_URI_TYPE_MAN ||
-		    yelp_uri_get_type (uri) == YELP_URI_TYPE_INFO) {
+		    yelp_uri_get_type (uri) == YELP_URI_TYPE_MAN) {
 			gtk_html_jump_to_anchor (priv->html_view,
                                                  yelp_uri_get_section (uri));
 			handled = TRUE;
