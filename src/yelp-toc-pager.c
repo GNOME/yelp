4cf04c5cc49ce69d4cde023a8b3e24662c13e61d
Brent Smith <bmsmith@src.gnome.org>
2006-06-25 21:57:36 +0000

    fix the biggest memory leak known to man; was using xmlFree() instead of
            * src/yelp-toc-pager.c: (create_toc_from_index): fix the biggest
            memory leak known to man; was using xmlFree() instead of xmlFreeDoc()

--- src/yelp-toc-pager.c
+++ src/yelp-toc-pager.c
@@ -825,7 +825,7 @@ create_toc_from_index (YelpTocPager *pager, gchar *index_file)
 
     xmlXPathFreeContext (xpath);
     xmlXPathFreeObject (objsect);
-    xmlFree (manindex_xml);
+    xmlFreeDoc (manindex_xml);
     
     return 1;
 }
