cc687827d8ba4b8bae369d0a7861d7fb9a2ece6a
Matthias Clasen <mclasen@redhat.com>
2010-06-30 09:43:56 -0400

    Adapt to recent glib api changes
    g_keyfile_settings_backend_new now takes 3 arguments, see bug 623154

--- src/yelp-application.c
+++ src/yelp-application.c
@@ -224,7 +224,7 @@ application_setup (YelpApplication *app)
         gchar *keyfile = g_build_filename (g_get_user_config_dir (),
                                            "yelp", "yelp.cfg",
                                            NULL);
-        priv->backend = g_keyfile_settings_backend_new (keyfile);
+        priv->backend = g_keyfile_settings_backend_new (keyfile, "/org/gnome/yelp", NULL);
         priv->gsettings = g_settings_new_with_backend ("org.gnome.yelp",
                                                        priv->backend);
         g_free (keyfile);
